d4c775c987fc8b2d888191609f24801e5cbf4388
josueBarretogit <josuebarretogit@gmail.com>
2024-04-26 09:26:32 -0500

    added inquire crate

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,55 @@
 use crate::generate_gitignore::generate::*;
 use crate::search_gitignore::search::*;
-use std::fs::{self, File};
 use std::path::Path;
 use std::process;
 
-use clap::Parser;
+use inquire::{Confirm, Select};
 
 mod generate_gitignore;
 mod search_gitignore;
 mod utils;
 
-#[derive(Parser, Debug)]
-#[command(version, about, long_about = None)]
-struct Args {
-    #[arg(short, long, default_value_t = false)]
-    manual: bool,
-    #[arg(short, long)]
-    lang: Option<String>,
-}
-
 #[tokio::main]
 async fn main() {
-    let args = Args::parse();
-
-    let is_manual = args.manual;
+    let lang_options = vec!["Rust", "Node", "Go", "Dart", "Java", "C", "Javascript"];
 
     if Path::new(".gitignore").exists() {
-        println!("a .gitignore already exists in the current directory");
+        let answer = Confirm::new("There is a .gitignore already, Do you want to overwrite it?")
+            .with_default(false)
+            .prompt();
+
+        match answer {
+            Ok(true) => {}
+            Ok(false) => {
+                println!("I will not touch your already existing .gitignore");
+                process::exit(0)
+            }
+            Err(e) => {
+                eprint!("There was an error, more details: {}", e);
+                process::exit(1)
+            }
+        }
+    }
+
+    let answer = match Select::new("Choose the language to get .gitignore for", lang_options)
+        .with_vim_mode(true)
+        .prompt()
+    {
+        Ok(choice) => choice,
+        Err(err) => {
+            eprint!("There was an error, more details: {err}");
+            process::exit(1)
+        }
+    };
+
+    println!("Getting your gitignore ...");
+
+    let search_result = search_for_gitignore(answer.to_string()).await;
+
+    if let Err(error) = search_result {
+        eprintln!("an error ocurred, details: {error}");
         process::exit(1);
     }
-
-    if !is_manual {
-        if let None = args.lang {
-            eprint!("A language was not provided");
-            process::exit(1);
-        }
-        println!("Getting your gitignore ...");
-
-        let search_result = search_for_gitignore(args.lang.unwrap()).await;
-
-        if let Err(error) = search_result {
-            eprintln!("an error ocurred, details: {error}");
-            process::exit(1);
-        }
-        println!(".gitignore generated in root dir");
-        process::exit(0)
-    }
-    generate_gitignore_manually();
+    println!(".gitignore generated in root dir");
+    process::exit(0)
 }
